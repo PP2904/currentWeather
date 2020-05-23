8990edd07de960224cb90542534628041f4322cb
P3ter <24958792+PP2904@users.noreply.github.com>
2020-05-23 14:09:23 +0200

    v2

--- weather_api_req.py
+++ weather_api_req.py
@@ -13,14 +13,3 @@ response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=<CityN
 # Print the status code of the response.
 data = response.json()
 print(data["main"]["temp"])
-
-
-
-
-{'coord': {'lon': 11.58, 'lat': 48.14}, 
-'weather': [{'id': 500, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
-'base': 'stations', 
-'main': {'temp': 23.2, 'feels_like': 19.66, 'temp_min': 21.67, 'temp_max': 25.56, 'pressure': 1019, 'humidity': 59}, 
-'wind': {'speed': 7.23, 'deg': 286}, 'rain': {'1h': 0.85}, 'clouds': {'all': 98}, 'dt': 1590234522, 
-'sys': {'type': 3, 'id': 2002112, 'country': 'DE', 'sunrise': 1590204309, 'sunset': 1590260164}, 
-'timezone': 7200, 'id': 2867714, 'name': 'Munich', 'cod': 200}
